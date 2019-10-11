cc99cb948845cfde256c77ba56c775be3c088efa
marilu <201318666@ingenieria.usac.edu.gt>
2019-10-11 17:46:52 -0600

    correcciones en db

---
+++ mysql/sql-scripts/CreateTable.sql
@@ -0,0 +1,5 @@
+CREATE TABLE usuarios (
+    nombre varchar(25),
+    apellido varchar(25),
+    departamento varchar(15)
+);
---
+++ mysql/sql-scripts/InsertData.sql
@@ -0,0 +1,19 @@
+INSERT INTO
+    usuarios (nombre, apellido, departamento)
+VALUES
+    ('marilu', 'duarte', 'IT');
+
+INSERT INTO
+    usuarios (nombre, apellido, departamento)
+VALUES
+    ('seles', 'duarte', 'IT');
+
+INSERT INTO
+    usuarios (nombre, apellido, departamento)
+VALUES
+    ('celeste', 'duarte', 'IT');
+
+INSERT INTO
+    usuarios (nombre, apellido, departamento)
+VALUES
+    ('marilu', 'amaya', 'IT');
