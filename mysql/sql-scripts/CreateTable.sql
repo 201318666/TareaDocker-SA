1a1ed40b6a577c479619491963eec48d4869cf4e
marilu <201318666@ingenieria.usac.edu.gt>
2019-11-05 18:10:22 -0600

    tarea finalizada

--- mysql/sql-scripts/CreateTable.sql
+++ mysql/sql-scripts/CreateTable.sql
@@ -1,3 +1,10 @@
+CREATE SCHEMA IF NOT EXISTS `tareadocker` DEFAULT CHARACTER SET utf8 ;
+USE `tareadocker` ;
+
+GRANT ALL PRIVILEGES ON tareadocker.* TO 'usuariobd'@'%';
+FLUSH PRIVILEGES;
+
+
 CREATE TABLE usuarios (
     nombre varchar(25),
     apellido varchar(25),
--- public_html/index.php
+++ public_html/index.php
@@ -1,13 +1,43 @@
 <h1>Tarea Containers 1/2</h1>
 <h4>Conectando a Database MySQL desde php...</h4>
-<?php 
-$host = 'mysql';
-$user = 'root';
-$pass = 'rootpassword';
-$conn = new mysqli($host, $user, $pass);
+<br>
+<form method="post">
+    <input type="text" name="nombre" placeholder="Ingresar nombre">
+    <br>
+    <input type="text" name="apellido" placeholder="Ingresar apellido">
+    <br>
+    <input type="text" name="departamento" placeholder="Ingresar departamento">
+    <input type="submit" name="submit" placeholder="Guardar">
+</form>
 
-if ($conn->connect_error) {
-    die("No se pudo conectar a MySQL: " . $conn->connect_error);
-} 
-echo "Conexion a MySQL exitosa!";
+<?php
+
+function guardar()
+{
+    $host = 'mysql';
+    $user = 'root';
+    $pass = 'rootpassword';
+    $conn = new mysqli($host, $user, $pass);
+
+    if ($conn->connect_error) {
+        die("No se pudo conectar a MySQL: " . $conn->connect_error);
+    } 
+    $nombre = $_POST["nombre"];
+    $apellido = $_POST["apellido"];
+    $departamento = $_POST["departamento"];
+    mysqli_select_db($conn,"tareadocker");
+    $sql = "INSERT INTO usuarios (nombre, apellido, departamento) VALUES ('$nombre', '$apellido', '$departamento')";
+    if(mysqli_query($conn, $sql)){
+        echo "Datos guardados.";
+    } else{
+        echo "ERROR: No se pudo guardar $sql. " . mysqli_error($conn);
+    }
+ 
+    // Close connection
+    mysqli_close($conn);
+}
+if(isset($_POST['submit']))
+{
+   guardar();
+}
 ?>
